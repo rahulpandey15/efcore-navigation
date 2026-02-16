aadb6a371ed1c1ba080aef88a91424d6d5c175ec
Rahul Pandey <rahulpandey862@gmail.com>
2026-02-16 11:19:56 +0530

    Added Product Controller for Change Tracking Demo

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -22,6 +22,10 @@ namespace efcore_navigation.Controllers
         public async Task<IActionResult> Get()
         {
             var department = await sampledbContext.Departments.ToListAsync(); // SELECT * FROM DEPARTMENTS
+
+            Console.WriteLine($"The current state of entity is {sampledbContext.Entry(department).State}");
+
+
             return Ok(department);
         }
 
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
+using System.Drawing;
 
 namespace efcore_navigation.Controllers
 {
@@ -17,6 +18,59 @@ namespace efcore_navigation.Controllers
         {
             this.dbContext = dbContext;
         }
+        [HttpPost]
+        public IActionResult Post()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Product p
+                 = new Product()
+                 {
+                     ProductName = "Mobile",
+                     CreatedBy = "Sytem",
+                     CreatedDate = DateTime.UtcNow
+                 };
+
+            Console.WriteLine($"The current state of product is ${dbContext.Entry(p).State}");
+
+
+            dbContext.Products.Add(p);
+
+            Console.WriteLine($"The current state of product is ${dbContext.Entry(p).State}");
+
+
+            dbContext.SaveChanges();
+
+
+            return Ok();
+        }
+
+
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+         Console.ForegroundColor = ConsoleColor.Red;
+
+            var product
+                  = dbContext.Products.AsNoTracking().Where(x => x.Id == id).FirstOrDefault();
+
+            Console.WriteLine($"The current state of Product Entity is {dbContext.Entry(product).State}");
+
+            product.ProductName = "Monitor";
+
+           Console.WriteLine($"The current state of Product Entity is {dbContext.Entry(product).State}");
+
+
+
+
+
+            return Ok(product);
+        }
+
+
 
         [HttpGet]
         public async Task<IActionResult> Get()
